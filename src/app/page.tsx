995f65db9b3779b1e7d223b6bd552db0d4c3e945
nataliafrancisca <nathifrancisca@gmail.com>
2024-11-19 17:25:51 -0300

    feat: import Search component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
+import Search from "./components/Search/Search";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <Search />
     </main>
   );
 }
