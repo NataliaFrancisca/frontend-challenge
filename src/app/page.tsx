268ade70a02a13bb739a9a6f3a1064610b64fe24
nataliafrancisca <nathifrancisca@gmail.com>
2024-11-19 23:26:53 -0300

    feat: fetch all characters to pass at view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
+import { fetchAllCharacters } from "./functions/fetchCharacter";
+
 import Header from "./components/Header/Header";
 import Search from "./components/Search/Search";
+import ViewCharacters from "./containers/ViewCharacters/ViewCharacters";
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetchAllCharacters();
+
   return (
     <main>
       <Header />
       <Search />
+      {response.results && <ViewCharacters characters={response.results}/> }
     </main>
   );
 }
