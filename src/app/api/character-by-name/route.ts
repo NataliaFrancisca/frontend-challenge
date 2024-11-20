130826cec74a52f575b5db95aa45fbec16fabbaf
nataliafrancisca <nathifrancisca@gmail.com>
2024-11-19 23:46:36 -0300

    feat: change URL to nameStartsWith because this way we can get more characters

--- src/app/api/character-by-name/route.ts
+++ src/app/api/character-by-name/route.ts
@@ -11,5 +11,5 @@ export async function GET(request: NextRequest){
         });
     };
 
-    return await httpRequest(`/characters?name=${characterName}&`);
+    return await httpRequest(`/characters?nameStartsWith=${characterName}&`);
 }
