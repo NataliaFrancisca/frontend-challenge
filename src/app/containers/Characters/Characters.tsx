9c95087f36fff13a5a611cc25b39b32cadad9fb7
nataliafrancisca <nathifrancisca@gmail.com>
2024-11-19 23:22:10 -0300

    feat: return list of characters

--- src/app/containers/Characters/Characters.tsx
+++ src/app/containers/Characters/Characters.tsx
@@ -1,40 +1,35 @@
-import Image from "next/image";
+'use client';
 import "./Characters.scss";
+import { useContext } from "react";
+import { SearchActionContext } from "@/app/context/SearchActionContext";
+import Loader from "@/app/components/Loader/Loader";
+import Character from "@/app/components/Character/Character";
+import { TCharacterBasicInfo } from "@/app/ts/types";
 
-type TCharacterBasicInfo = {
-    name: string,
-    id: number,
-    thumbnail: {
-        extension: string,
-        path: string
+const Characters = (props: {characters: Array<TCharacterBasicInfo> | null}) => {
+
+    const { status } = useContext(SearchActionContext);
+
+    if(status){
+        return(
+            <main className="characters__grid full-width">
+                <Loader />
+            </main>
+        )
     }
-}
 
-const Characters = (props: {characters: Array<TCharacterBasicInfo>}) => {
+    if(props.characters == null){
+        return(
+            <main className="characters__grid full-width">
+                <h2 className="h2__not-found">Nenhum herói foi encontrado</h2>
+            </main>
+        )
+    }
 
     return(
         <main className="characters__grid">
-
             {props.characters.map((character) => (
-                <article className="article__character">
-                    <Image 
-                        width={1000}
-                        height={1000}
-                        alt={character.name}
-                        className="img__character"
-                        src={`${character.thumbnail.path}.${character.thumbnail.extension}`} 
-                    />
-
-                    <div className="div__character-details">
-                        <h2>{character.name}</h2>
-                        <Image 
-                            src={"/icon/heart/heart.svg"} 
-                            width={10} 
-                            height={10} 
-                            alt="heroi"
-                        />
-                    </div>
-                </article>
+                <Character character={character} key={character.id} />
             ))}
         </main>
     )
