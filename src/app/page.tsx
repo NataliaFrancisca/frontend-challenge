5dfb3b81dd85842331fc69407c0010d59a27c7e4
nataliafrancisca <nathifrancisca@gmail.com>
2024-11-21 00:23:25 -0300

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,13 @@ import ViewCharacters from "./containers/ViewCharacters/ViewCharacters";
 
 export default async function Home() {
   const response = await fetchAllCharacters();
-
+  
   return (
     <main>
       <Header />
       <Search />
       {response.results && <ViewCharacters characters={response.results}/> }
+      <footer></footer>
     </main>
   );
 }
