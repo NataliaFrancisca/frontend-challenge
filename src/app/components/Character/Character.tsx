bd59700c7986dbb6e20bf63ee885e72041d5abb6
nataliafrancisca <nathifrancisca@gmail.com>
2024-11-21 16:30:19 -0300

    feat: replace label to the HeartButton

--- src/app/components/Character/Character.tsx
+++ src/app/components/Character/Character.tsx
@@ -3,36 +3,29 @@ import Image from "next/image";
 
 import "./Character.scss";
 import { TCharacterBasicInfo } from "@/app/ts/types";
-import { useCharacter } from "./useCharacter";
 import Link from "next/link";
+import HeartButton from "../HeartButton/HeartButton";
 
 const Character = (props: {character: TCharacterBasicInfo }) => {
     const { name, thumbnail } = props.character;
-    const { isFav, onToggleFavorite } = useCharacter(props.character);
 
     return(
-        <Link className="article__character" href={`/heroi/${props.character.id}`}>
-            <Image 
-                width={1000}
-                height={1000}
-                alt={name}
-                className="img__character"
-                src={`${thumbnail.path}.${thumbnail.extension}`} 
-            />
-
+        <article className="article__character" >
+            <Link href={`/heroi/${props.character.id}`}>
+                <Image 
+                    width={1000}
+                    height={1000}
+                    alt={name}
+                    className="img__character"
+                    src={`${thumbnail.path}.${thumbnail.extension}`} 
+                />
+            </Link>
+          
             <div className="div__character-details">
                 <h2>{name}</h2>
-                <label className="label__switch-favorite">
-                    <input 
-                        type="checkbox" 
-                        id="filter-favorites"
-                        checked={isFav}
-                        onChange={() => onToggleFavorite()}
-                    />
-                    <span className="icon" />
-                </label>
+                <HeartButton character={props.character} />
             </div>
-        </Link>
+        </article>
     )
 }
 
