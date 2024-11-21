e427c0c23f6116a884b03db6c56249e34af2bc7f
nataliafrancisca <nathifrancisca@gmail.com>
2024-11-21 17:42:41 -0300

    feat: remove server component to client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,38 @@
+"use client";
 import { fetchAllCharacters } from "./functions/fetchCharacter";
 
 import Header from "./components/Header/Header";
 import Search from "./components/Search/Search";
 import ViewCharacters from "./containers/ViewCharacters/ViewCharacters";
+import { TCharacterBasicInfo } from "./ts/types";
+import { useEffect, useState } from "react";
+import Loader from "./components/Loader/Loader";
 
-export default async function Home() {
-  const response = await fetchAllCharacters();
+export default function Home() {
+
+
+  const [characters, setCharacters] = useState<TCharacterBasicInfo[] | null>(null);
+
+  const getAllCharacters = async() => {
+    const response = await fetchAllCharacters();
+
+    if(response.results){
+      setCharacters(response.results);
+    }
+  }
+
+  useEffect(() => {
+    getAllCharacters();
+  },[]);
+  
+  
   
   return (
-    <main>
+    <main className="main__page">
       <Header />
       <Search />
-      {response.results && <ViewCharacters characters={response.results}/> }
+      {characters == null && <Loader />}
+      {characters && <ViewCharacters characters={characters} /> }
       <footer></footer>
     </main>
   );
