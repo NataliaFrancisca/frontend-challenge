791216fdea612c7c4c3f98d6e9f57eba15f53f37
nataliafrancisca <nathifrancisca@gmail.com>
2024-11-21 00:21:39 -0300

    feat: create custom hook to handle character component logic

---
+++ src/app/components/Character/useCharacter.tsx
@@ -0,0 +1,25 @@
+import { isFavorite, toggleFavorite } from "@/app/storage/fav";
+import { TCharacterBasicInfo } from "@/app/ts/types";
+import { useEffect, useState } from "react";
+
+export const useCharacter = (character: TCharacterBasicInfo) => {
+    const [isFav, setIsFav] = useState(false);
+
+    const onToggleFavorite = async() => {
+        const response = await toggleFavorite(character as TCharacterBasicInfo);
+        if(response){
+            setIsFav(!isFav);
+        }
+    }
+
+    const checkIsAlreadyFavorite = async() => {
+        const isAlreadFavorite = await isFavorite(character);
+        setIsFav(isAlreadFavorite);
+    }
+    
+    useEffect(() => {
+        checkIsAlreadyFavorite();
+    },[]);
+
+    return { isFav, onToggleFavorite }
+};
